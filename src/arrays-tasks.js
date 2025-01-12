84f0c3f6400e110841d4622b5738f94bf033b424
XedDevi1 <vadim.presman@gmail.com>
2025-01-12 21:56:50 +0100

    feat: add implementation for generateOdds method

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -360,8 +360,12 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) {
+    return [];
+  }
+
+  return Array.from({ length: len }, (nothing, index) => 2 * index + 1);
 }
 
 /**
